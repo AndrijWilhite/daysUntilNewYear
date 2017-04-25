1bc4f54fbe26d77befb05db7478365ddfa9384d9
Andrij Wilhite <andrij@Andrijs-MacBook-Pro.local>
2017-04-25 14:45:48 -0700

    first push

---
+++ daysUntilNewYear-gregorian.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+
+function getDaysUntillNewYear(){
+     var current = parseInt(moment().format('DDD'));
+     var untilYear = moment().format('YYYY');
+     var yearVal = moment([untilYear]).isLeapYear();
+        if(yearVal === false){
+            var daysUntil = 365 - current;
+          } else{
+            var daysUntil = 366 - current;
+         }
+        return daysUntil;
+}
+module.exports = getDaysUntillNewYear;
